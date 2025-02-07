738ef52b42c0803c6896363e90829da57ffdf554
Masashi Kawafuji <masashikawafuji@proton.me>
2025-02-08 01:46:59 +0900

    Fix inconsistent type definition for functions

--- src/providers/YouTubeProvider.tsx
+++ src/providers/YouTubeProvider.tsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 
 declare global {
   interface Window {
-    onYouTubeIframeAPIReady: () => void;
+    onYouTubeIframeAPIReady(): void;
   }
 }
 
